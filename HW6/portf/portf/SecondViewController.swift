63413c04adf48913bb38547a7744ea0330d85353
Margocan <m.samoilovacan@gmail.com>
2024-01-16 00:43:08 +0300

    Redo Homework6: 1) Change constrains ThirdVC main label "Hard skills" 2) Change to Final Class 3) Remove magic numbers (ViewController, MainStoryboard) 4) Changed structure of code

--- HW6/portf/portf/SecondViewController.swift
+++ HW6/portf/portf/SecondViewController.swift
@@ -2,19 +2,18 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+final class SecondViewController: UIViewController {
     
+    @IBOutlet private weak var labelSkills: UILabel!
+    @IBOutlet private weak var labelFirst: UILabel!
+    @IBOutlet weak var labelSecond: UILabel!
+    @IBOutlet private weak var labelThird: UILabel!
+    @IBOutlet weak var nextsecondButton: UIButton!
     
     @IBAction private func pushDidTap () {
-        let mainStoryboard = UIStoryboard (name: "Main", bundle: nil)
+        let mainStoryboard = UIStoryboard (name: "Profile", bundle: nil)
         let thirdVC = mainStoryboard.instantiateViewController(withIdentifier: "ThirdVC")
         navigationController?.pushViewController(thirdVC, animated: true)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-       
-    }
-    
-
+   
 }
--- HW6/portf/portf/ThirdViewController.swift
+++ HW6/portf/portf/ThirdViewController.swift
@@ -2,20 +2,18 @@
 
 import UIKit
 
-class ThirdViewController: UIViewController {
+final class ThirdViewController: UIViewController {
     
+    @IBOutlet private weak var labelHardSkills: UILabel!
+    @IBOutlet private weak var labelXcode: UILabel!
+    @IBOutlet private weak var labelFigma: UILabel!
+    @IBOutlet private weak var labelGit: UILabel!
+    @IBOutlet weak var homeButton: UIButton!
     
     @IBAction private func pushDidTap () {
-        let mainStoryboard = UIStoryboard (name: "Main", bundle: nil)
+        let mainStoryboard = UIStoryboard (name: "Profile", bundle: nil)
         let mainVC = mainStoryboard.instantiateViewController(withIdentifier: "MainVC")
         navigationController?.pushViewController(mainVC, animated: true)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-      
-    }
-
-
-
+    
 }
